8d24c950d911f6526ed1a29765d5f92e51e967b8
Marclap <jhona.cb991128@gmail.com>
2022-10-06 01:04:09 -0500

    Fix heroku app

---
+++ config.py
@@ -0,0 +1,11 @@
+class DevelopmentConfig():
+  DEBUG = True
+  MYSQL_HOST = 'sql10.freemysqlhosting.net'
+  MYSQL_USER = 'sql10524607'
+  MYSQL_PASSWORD = 'ghJ4pVGwnd'
+  MYSQL_DB = 'sql10524607'
+  
+
+config = {
+  'development': DevelopmentConfig  
+}
